11d371c5872cc441b53738facde1d82157f61931
RonitBanga <bangaronit22@gmail.com>
2024-01-21 00:38:39 +0530

    Jan 20, 2024

---
+++ 1207. Unique Number of Occurrences.java
@@ -0,0 +1,12 @@
+//1207. Unique Number of Occurrences
+
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        Map<Integer, Integer> count = new HashMap<>();
+        for (int a : arr)
+            count.put(a, 1 + count.getOrDefault(a, 0));
+                    System.out.println(count.values());
+
+        return count.size() == new HashSet<>(count.values()).size();
+    }
+}
