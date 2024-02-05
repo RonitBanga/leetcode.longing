b3030e2792fa602ff6754312f745a8f65d74cd9a
RonitBanga <bangaronit22@gmail.com>
2024-02-05 23:42:57 +0530

    Feb 5, 2024

---
+++ 387. First Unique Character in a String.java
@@ -0,0 +1,16 @@
+// 387. First Unique Character in a String
+
+class Solution {
+    public int firstUniqChar(String s) {
+        int freq[] = new int[26];
+        for(Character c : s.toCharArray()) {
+            freq[c-'a']++;
+        }
+        for(int i=0; i<s.length(); i++) {
+            if(freq[s.charAt(i)-'a']==1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
