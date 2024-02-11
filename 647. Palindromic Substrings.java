a1cde1ebf2ca50fb902e3eca478f0fdc4c71824d
RonitBanga <bangaronit22@gmail.com>
2024-02-11 15:23:11 +0530

    Feb 11, 2024

--- 647. Palindromic Substrings.java
+++ 647. Palindromic Substrings.java
@@ -39,3 +39,60 @@ class Solution {
         return count;
     }
 }
+
+// Approach 2
+
+class Solution {
+    int count;
+
+    void palin(String s, int i, int j, int n) {
+        while(i>=0 && j<n && s.charAt(i)==s.charAt(j)) {
+            count++;
+            i--;
+            j++;
+        }
+    }
+
+    public int countSubstrings(String s) {
+        int n = s.length();
+        count = 0;
+    
+
+    for(int i=0; i<n; i++) {
+        palin(s, i, i, n);
+        palin(s, i, i+1, n);
+    }
+
+    return count;
+    }
+}
+
+// Approach 3
+
+class Solution {
+    public int countSubstrings(String s) {
+        int n = s.length();
+
+        boolean dp[][] = new boolean[n][n];
+
+        int count = 0;
+
+        for(int L=1; L<=n; L++) {
+            for(int i=0; i+L<=n; i++) {
+                int j = i+L-1;
+
+                if(i==j) {
+                    dp[i][j] = true;
+                }else if(i+1==j) {
+                    dp[i][j] = (s.charAt(i)==s.charAt(j));
+                }else {
+                    dp[i][j] = (s.charAt(i)==s.charAt(j) && dp[i+1][j-1]);
+                }
+
+                if(dp[i][j]==true) count++;
+            }
+        }
+
+        return count;
+    }
+}
