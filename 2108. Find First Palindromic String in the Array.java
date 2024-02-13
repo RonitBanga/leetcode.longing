326e84144466a9a27c21efb07a8d2de80cbb469d
RonitBanga <bangaronit22@gmail.com>
2024-02-13 23:44:17 +0530

    Feb 13, 2024

--- 2108. Find First Palindromic String in the Array.java
+++ 2108. Find First Palindromic String in the Array.java
@@ -1,1 +1,21 @@
 // 2108. Find First Palindromic String in the Array
+
+class Solution {
+    boolean isPalindrom(String s) {
+        int j = s.length()-1;
+        int i = 0;
+        while(i<j) {
+            if(s.charAt(i++)!=s.charAt(j--)) {
+                return false;
+            } 
+        }
+        return true;
+    }
+    public String firstPalindrome(String[] words) {
+        int n =  words.length;
+        for(int i=0; i<n; i++) {
+            if(isPalindrom(words[i])) return words[i];
+        }
+        return "";
+    }
+}
