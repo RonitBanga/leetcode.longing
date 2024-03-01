3b921daa8a997767c5651ab96085a596a2faec1c
RonitBanga <bangaronit22@gmail.com>
2024-03-01 23:25:32 +0530

    Mar 01, 2024

---
+++ 2864. Maximum Odd Binary Number.java
@@ -0,0 +1,16 @@
+2864. Maximum Odd Binary Number
+
+class Solution {
+    public String maximumOddBinaryNumber(String s) {
+        int oneCount = -1;
+        StringBuilder ans = new StringBuilder();
+        for (int i = 0; i < s.length(); ++i) {
+            oneCount += (s.charAt(i) == '1') ? 1 : 0;
+        }
+        for (int i = 0; i < s.length(); i++, oneCount--) {
+            ans.append(oneCount > 0 ? '1' : '0');
+        }
+        ans.deleteCharAt(ans.length() - 1);
+        return ans.append('1').toString();
+    }
+}
