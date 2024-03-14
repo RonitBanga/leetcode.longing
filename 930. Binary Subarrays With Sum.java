666bfa025fa84ce842771a984f97aee6fb9fcb86
RonitBanga <bangaronit22@gmail.com>
2024-03-14 23:23:44 +0530

    Mar 14, 2024

---
+++ 930. Binary Subarrays With Sum.java
@@ -0,0 +1,15 @@
+// 930. Binary Subarrays With Sum
+
+class Solution {
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        int psum = 0, res = 0, count[] = new int[nums.length + 1];
+        count[0] = 1;
+        for (int i : nums) {
+            psum += i;
+            if (psum >= goal)
+                res += count[psum - goal];
+            count[psum]++;
+        }
+        return res;
+    }
+}
