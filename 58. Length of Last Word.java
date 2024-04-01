e7d9dc87ea156ced43b049e448c0c23853a299dc
RonitBanga <bangaronit22@gmail.com>
2024-04-01 21:45:47 +0530

    Apr 01, 2024

---
+++ 58. Length of Last Word.java
@@ -0,0 +1,16 @@
+// 58. Length of Last Word
+
+class Solution {
+    public int lengthOfLastWord(String s) {
+        String str = s.trim();
+        int n = str.length();
+        int count = 0;
+
+        for(int i=n-1; i>=0; i--) {
+            if(str.charAt(i)==' ') break;
+            count++;
+        }
+
+        return count;
+    }
+}
