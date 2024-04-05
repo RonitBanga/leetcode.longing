f3f81a8307d1dffb99c1c066e2018c4e085a2985
RonitBanga <bangaronit22@gmail.com>
2024-04-05 07:30:09 +0530

    Apr 05, 2024

--- 1544. Make The String Great.java
+++ 1544. Make The String Great.java
@@ -1,20 +1,6 @@
-// 1614. Maximum Nesting Depth of the Parentheses
+// 1544. Make The String Great
 
 class Solution {
-    public int maxDepth(String s) {
-        int count = 0;
-        int maxNum = 0;
-        for (char c : s.toCharArray()) {
-            if (c == '(') {
-                count++;
-                if (maxNum < count)
-                    maxNum = count;
-            } else if (c == ')') {
-                count--;
-            }
-        }
-        return maxNum;
-    }class Solution {
     public String makeGood(String s) {
         Stack<Character> stack = new Stack<>();
 
@@ -34,5 +20,4 @@ class Solution {
         return str.toString();
         
     }
-}
 }
