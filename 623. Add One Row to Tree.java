eede497e496571ff5b26598cc7755e6464ca4c75
RonitBanga <bangaronit22@gmail.com>
2024-04-16 23:30:49 +0530

    Apr 16, 2024

---
+++ 623. Add One Row to Tree.java
@@ -0,0 +1,50 @@
+// 623. Add One Row to Tree
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    public TreeNode solve(TreeNode root, int val, int depth, int curr) {
+        if(root == null) return null;
+
+        if(curr==depth-1) {
+            TreeNode l = root.left;
+            TreeNode r = root.right;
+
+            root.left = new TreeNode(val);
+            root.right = new TreeNode(val);
+            root.left.left = l;
+            root.right.right = r;
+
+            return root;
+        }
+
+        root.left = solve(root.left, val, depth, curr + 1);
+        root.right = solve(root.right, val, depth, curr + 1);
+
+        return root;
+    }
+
+    public TreeNode addOneRow(TreeNode root, int val, int depth) {
+        if(depth==1) {
+            TreeNode node = new TreeNode(val);
+            node.left = root;
+            return node;
+        }
+
+        return solve(root, val, depth, 1);
+    }
+}
