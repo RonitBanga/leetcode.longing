23699bfdb0cc88af83f8ba190e36abaaf6707d90
RonitBanga <bangaronit22@gmail.com>
2024-04-30 00:01:04 +0530

    Apr 30, 2024

--- 2997. Minimum Number of Operations to Make Array XOR Equal to K.java
+++ 2997. Minimum Number of Operations to Make Array XOR Equal to K.java
@@ -1,3 +1,13 @@
-public class 2997. Minimum Number of Operations to Make Array XOR Equal to K {
-    
+// 2997. Minimum Number of Operations to Make Array XOR Equal to K
+
+class Solution {
+    public int minOperations(int[] nums, int k) {
+        int totalXor = 0;
+        for (int num : nums) {
+            totalXor ^= num;
+        }
+        
+        int diff = (totalXor ^ k);
+        return Integer.bitCount(diff);
+    }
 }
